9570deffd57f993f77b2be15516c35e71a08e583
Kezia Yusika <keziayusika05@gmail.com>
2023-06-13 14:38:44 +0700

    implement crud collection

---
+++ src/handlers/collectionHandlers.js
@@ -0,0 +1,85 @@
+const connectDb = require("../lib/dbConnection");
+const { uuidv4 } = require("@firebase/util");
+
+const getCollectionHandler = async (request, h) => {
+    const userId = request.params.userId;
+    let response;
+
+    let query = `SELECT * FROM user_plant_collection`;
+    if (userId) {
+        query += ` WHERE user_id = '${userId}'`;
+    }
+
+    const res = await connectDb(query);
+    const collection = res.rows;
+    console.log(collection);
+
+    if (collection !== undefined) {
+        response = h.response({
+            status: "success",
+            message: "User collection found",
+            data: collection,
+        }).code(200);
+    } else {
+        response = h.response({
+            status: "fail",
+            message: "User collection not found"
+        }).code(404);
+    }
+
+    return response;
+};
+
+
+const addCollectionHandler = async (request, h) => {
+    const userId  = request.params.userId;
+    const { plantId } = request.payload;
+    let response;
+
+    const collectionId = uuidv4();
+    console.log("plantId", plantId);
+
+    const query = `INSERT INTO user_plant_collection VALUES('${collectionId}','${userId}','${plantId}', NOW(), NOW(), NULL);`;
+    const res = await connectDb(query);
+
+    if (res && res.rowCount > 0) {
+        const getUserCollection = await connectDb(`SELECT * FROM user_plant_collection WHERE user_id = '${userId}'::uuid`);
+        const userCollection = getUserCollection.rows[0];
+        response = h.response({
+            status: "success",
+            message: "Successfully add plant to collection",
+            data: userCollection,
+        }).code(200);
+    } else {
+        response = h.response({
+            status: "fail",
+            message: "Failed to add plant to collection",
+        }).code(400);
+    }
+
+    return response;
+};
+
+const deleteCollectionHandler = async (request, h) => {
+    const id  = request.params.collectionId;
+    let response;
+
+    const query = `DELETE FROM USER_PLANT_COLLECTION WHERE id = '${id}';`;
+    const res = await connectDb(query);
+
+    if (res && res.rowCount > 0) {
+        response = h.response({
+            status: "success",
+            message: "Successfully delete plant from collection",
+        }).code(200);
+    } else {
+        response = h.response({
+            status: "fail",
+            message: "Failed to delete plant from collection",
+        }).code(400);
+    }
+
+    return response;
+};
+
+module.exports = { getCollectionHandler, addCollectionHandler, deleteCollectionHandler };
