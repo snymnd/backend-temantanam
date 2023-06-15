9cebeacd882ec613905c7835f68e95494c1aa31e
Kezia Yusika <keziayusika05@gmail.com>
2023-06-15 19:47:41 +0700

    correction in get plant

--- src/handlers/userHandlers.js
+++ src/handlers/userHandlers.js
@@ -2,96 +2,89 @@
 const connectDb = require("../lib/dbConnection");
 
 const getUserHandler = async (request, h) => {
-<<<<<<< HEAD
-    const id  = request.params.userId;
-=======
-    const { id } = request.params;
->>>>>>> 9338d39 (implement create update users)
-    let response;
+  const id = request.params.userId;
+  let response;
 
-    let query = `SELECT * FROM users`;
-    if (id) {
-<<<<<<< HEAD
-        query += ` WHERE id = '${id}'`;
-=======
-        query += ` WHERE id = ${id}`;
->>>>>>> 9338d39 (implement create update users)
-    }
+  let query = `SELECT * FROM users`;
+  if (id) {
+    query += ` WHERE id = '${id}'`;
+  }
 
-    const res = await connectDb(query);
-    const user = res.rows[0];
-    console.log(user);
+  const res = await connectDb(query);
+  const user = res.rows[0];
+  console.log(user);
 
-    if (user !== undefined) {
-        response = h.response({
-            status: "success",
-            message: "User found",
-            data: user,
-        }).code(200);
-    } else {
-        response = h.response({
-            status: "fail",
-            message: "User not found"
-<<<<<<< HEAD
-        }).code(404);
-=======
-        }).code(400);
->>>>>>> 9338d39 (implement create update users)
-    }
+  if (user !== undefined) {
+    response = h
+      .response({
+        status: "success",
+        message: "User found",
+        data: user,
+      })
+      .code(200);
+  } else {
+    response = h
+      .response({
+        status: "fail",
+        message: "User not found",
+      })
+      .code(404);
+  }
 
-    return response;
+  return response;
 };
 
 const updateUserHandler = async (request, h) => {
-    const id  = request.params.userId;
-    const { name, email, image_url, phone } = request.payload;
-    let response;
+  const id = request.params.userId;
+  const { name, email, image_url, phone } = request.payload;
+  let response;
 
-    console.log("name:", name);
-    console.log("email:", email);
-    console.log("image_url:", image_url);
-    console.log("phone:", phone);
-    console.log(id)
+  console.log("name:", name);
+  console.log("email:", email);
+  console.log("image_url:", image_url);
+  console.log("phone:", phone);
+  console.log(id);
 
-    let query = `UPDATE users SET`;
-    if (name !== undefined) {
-        query += ` name = '${name}',`;
-    }
-    if (email !== undefined) {
-        query += ` email = '${email}',`;
-    }
-    if (image_url !== undefined) {
-        query += ` image_url = '${image_url}',`;
-    }
-    if (phone !== undefined) {
-        query += ` phone = '${phone}',`;
-    }
+  let query = `UPDATE users SET`;
+  if (name !== undefined) {
+    query += ` name = '${name}',`;
+  }
+  if (email !== undefined) {
+    query += ` email = '${email}',`;
+  }
+  if (image_url !== undefined) {
+    query += ` image_url = '${image_url}',`;
+  }
+  if (phone !== undefined) {
+    query += ` phone = '${phone}',`;
+  }
 
-    query += ` updated_at = NOW() WHERE id = '${id}'::uuid`;
-    console.log("query:", query);
-    const res = await connectDb(query);
+  query += ` updated_at = NOW() WHERE id = '${id}'::uuid`;
+  console.log("query:", query);
+  const res = await connectDb(query);
 
-    if (res && res.rowCount > 0) {
-        const getUser = await connectDb(`SELECT * FROM users WHERE id = '${id}'::uuid`);
-        const updatedUser = getUser.rows[0];
-        response = h.response({
-            status: "success",
-            message: "User updated successfully",
-            data: updatedUser,
-        }).code(200);
-    } else {
-        response = h.response({
-            status: "fail",
-            message: "Failed to update user",
-        }).code(400);
-    }
+  if (res && res.rowCount > 0) {
+    const getUser = await connectDb(
+      `SELECT * FROM users WHERE id = '${id}'::uuid`
+    );
+    const updatedUser = getUser.rows[0];
+    response = h
+      .response({
+        status: "success",
+        message: "User updated successfully",
+        data: updatedUser,
+      })
+      .code(200);
+  } else {
+    response = h
+      .response({
+        status: "fail",
+        message: "Failed to update user",
+      })
+      .code(400);
+  }
 
-    return response;
+  return response;
 };
 
-
-<<<<<<< HEAD
 module.exports = { getUserHandler, updateUserHandler };
-=======
-module.exports = { getUserHandler, updateUserHandler };
->>>>>>> 9338d39 (implement create update users)
--- src/plugins/user.js
+++ src/plugins/user.js
@@ -9,19 +9,9 @@ module.exports = userPlugin = {
     server.route([
       {
         method: "GET",
-<<<<<<< HEAD
         path: "/user/{userId}",
         config: {
           // auth: false,
-=======
-        path: "/user/{userId?}",
-        config: {
-<<<<<<< HEAD
-          auth: false,
->>>>>>> 9338d39 (implement create update users)
-=======
-          // auth: false,
->>>>>>> e0ae3e0 (implement auth for cu users)
         },
         handler: getUserHandler,
       },
@@ -29,15 +19,7 @@ module.exports = userPlugin = {
         method: "PUT",
         path: "/user/update/{userId}",
         config: {
-<<<<<<< HEAD
-<<<<<<< HEAD
           // auth: false,
-=======
-          auth: false,
->>>>>>> 9338d39 (implement create update users)
-=======
-          // auth: false,
->>>>>>> e0ae3e0 (implement auth for cu users)
         },
         handler: updateUserHandler,
       },
