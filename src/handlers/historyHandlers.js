e7b42ade61757816ddad330c6df618938017788e
Kezia Yusika <keziayusika05@gmail.com>
2023-06-16 10:11:52 +0700

    corretion in response

--- src/handlers/historyHandlers.js
+++ src/handlers/historyHandlers.js
@@ -5,7 +5,7 @@ const getHistoryHandler = async (request, h) => {
     const userId = request.params.userId;
     let response;
 
-    let query = `SELECT h.id AS history_id, h.created_at AS created_at, p.id AS plant_id, p.name AS plant_name, p.species AS plant_species, p.image_url AS plant_image_url
+    let query = `SELECT h.id AS history_id, h.created_at AS created_at, p.id AS plant_id, p.name AS plant_name, p.species AS plant_species, p.image_url AS plant_image_url, p.classification AS plant_classification
                 FROM history h
                 JOIN plant p ON h.plant_id = p.id`
     if (userId) {
